081c706af44bfb41a2eec2159f350a924ddf0e9d
SiyaLab <ntando.mndawe2@gmail.com>
2023-06-26 18:00:27 +0200

    rm local demo creds

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ load_dotenv()  # Load variables from .env file
 TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
 
 # MySQL database credentials
-# url: 'mysql://root:zxaS68MF@localhost:3306/ThengaDB_Dev',
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
